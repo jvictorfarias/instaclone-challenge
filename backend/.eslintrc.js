4c829a8cbe5f1d99f032a74df9dfc145a923a9a3
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-11-22 16:55:13 -0300

    environment: ready

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-method-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
